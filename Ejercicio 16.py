eec80d8bb10ec24993b73a76b3e2a0ddac7dd28f
jfNatanahel <natanahel.jf@gmail.com>
2023-08-24 22:03:43 -0300

    Update Ejercicio 16.py

--- Ejercicio 16.py
+++ Ejercicio 16.py
@@ -4,12 +4,12 @@ while i<=n:
     x=int(input("Ingresar numero de la lista: "))
     x1=x
     y=0
-    while x1<0:
+    while x1!=0:
         d=x1 % 10
         y=y*10+d
         x1=x1//10
     if x!=y:
-        print("El numero que no es capicua es:")
+        print("El numero que no es capicua es:",x)
     else:
         print("El numero es capicua")
     i=i+1
