245bf8ea0ea6675a06eb892c8e02a0f536152764
jfNatanahel <natanahel.jf@gmail.com>
2023-09-01 15:40:47 -0300

    (PRACTICA AULA)

---
+++ Ejercicio (PRACTICA AULA).py
@@ -0,0 +1,44 @@
+#<>
+dni=int(input("Ingresar el numero de dni: "))
+sexo=int(input("Ingresar el sexo 0=femenino/1=masculino: "))
+longitud=0
+s=0
+dni_aux=dni
+ultimo_digito=dni%10
+while dni>0:
+    r=dni %10
+    dni=dni //10
+    longitud= longitud+1
+if longitud>8:
+    print("EL DNI SUPERA LOS 8 DIGITOS")
+else:
+    if dni_aux % 2 ==0:
+        ultimo_digito=ultimo_digito+2
+    else:
+        ultimo_digito=ultimo_digito+1
+    if ultimo_digito>=10:
+        while ultimo_digito>0:
+            r1=ultimo_digito%10
+            s=s+r1
+            ultimo_digito=ultimo_digito//10
+        ultimo_digito=s
+    if sexo==0:
+        f_dni_nuevo=27
+        dividendo=10000000
+        while dni_aux>0:
+            cociente=dni_aux // dividendo
+            dni_aux=dni_aux%dividendo
+            dividendo=dividendo//10
+            f_dni_nuevo=f_dni_nuevo*10+cociente
+        print("Cuit es:",f_dni_nuevo*10+ultimo_digito)
+    if sexo==1:
+        m_dni_nuevo=20
+        dividendo=10000000
+        while dni_aux>0:
+            cociente=dni_aux // dividendo
+            dni_aux=dni_aux%dividendo
+            dividendo=dividendo//10
+            m_dni_nuevo=m_dni_nuevo*10+cociente
+        print("Cuit es:",m_dni_nuevo*10+ultimo_digito)
+
+    
