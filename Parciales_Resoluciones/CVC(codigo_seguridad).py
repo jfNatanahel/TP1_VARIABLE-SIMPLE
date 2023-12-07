4f2e3890d98af14b8f7b95af4805b67d8e4e267d
jfNatanahel <natanahel.jf@gmail.com>
2023-12-06 23:24:22 -0300

    revisado

--- Parciales_Resoluciones/CVC(codigo_seguridad).py
+++ Parciales_Resoluciones/CVC(codigo_seguridad).py
@@ -1,63 +1,64 @@
-#DNI=22555444
-#TARJETA CREDITO= 4348577015644896
+#DNI=
+#TARJETA DE CREDITO=4348577015644895
 bandera=0
-
 while bandera==0:
-    contador_dni=0
-    contador_tarjeta=0
-    dni=int(input("Ingresar su dni: "))
+    dni=int(input("Ingresar el dni: "))
+    tarjeta_credito=int(input("Ingresar tarjeta de credito: "))
+    dni_digitos=0
+    tarjeta_digito=0
     aux_dni=dni
-    tarjeta_credito=int(input("Ingresar su numero de tarjeta de credito: "))
+    aux2_dni=dni
+    aux_tarjeta=tarjeta_credito
+    aux2_tarjeta=tarjeta_credito
     while dni>0:
         r1=dni %10
-        contador_dni+=1
-        dni=dni//10
+        dni_digitos+=1
+        dni=dni //10
     while tarjeta_credito>0:
         r2=tarjeta_credito%10
-        contador_tarjeta+=1
+        tarjeta_digito+=1
         tarjeta_credito=tarjeta_credito//10
-    
-    if contador_dni != 8 and contador_tarjeta !=16:
-        validacion=input("DNI o Tarjeta de credito es incorrecto ¡INTENTAR DE NUEVO! apretar S mayuscula: ")
-        if validacion=="S":
-            bandera=0
-    else:
-            bandera=1
-    
-suma_a=0
-ud=dni%10
-while dni>0:
-    r=dni %10
-    suma_b=suma_b+r
-    dni=dni//10
+    if dni_digitos== 8 and tarjeta_digito==16:
+        print("Ambos numeros son validos")
+        bandera=1
+    else: 
+        print("Algunos de los numeros ingresados es invalido. Intente de nuevo")
+        bandera=0
+#3)a. Se suman todos los digitos del DNI
+suma_dni=0
+while aux_dni>0:
+    r3= aux_dni % 10
+    suma_dni=suma_dni+r3
+    aux_dni=aux_dni//10
+#print("a",suma_dni)
+#3)b. Sumar los digitos posiciones impares desde la 3 posicion.
 i=17
-suma_b=0
-while i<=3:
-    r=tarjeta_credito % 10
+suma_tarjeta=0
+while i>=3:
+    r4=aux_tarjeta%10
+    aux_tarjeta=aux_tarjeta//10
     i=i-1
-    if i % 2 != 0:
-        suma_b=suma_b+r
-    tarjeta_credito=tarjeta_credito//10
-ss=suma_a//16
-t=suma_b//16
+    if i %2!=0:
+        suma_tarjeta=suma_tarjeta+r4
+#print("b",suma_tarjeta)
+#3)c. El CVC se calcula en combinacion de tres variables (UD,SS Y T)
+ud=aux2_dni %10
+ss=suma_dni//16
+t=suma_tarjeta//16
 cvc=((ud*10+ss)*10+t)
+print("El CVC es:",cvc)
+#4) Determinar de acuerdo a los dos primeros digitos de la tarjeta cual es la entidad emisora
 j=0
 divisor=1000000000000000
-while j<=2:
-    r=aux_dni % divisor
-    c=aux_dni // divisor
-    j=j+1
+primeros_digitos=0
+while j<2:
+    cociente=aux2_tarjeta // divisor
+    r=dni%10
     divisor=divisor//10
-if c==34 or c==37:
-    print("AMERICAN EXPRESS")
-if c==51 or c==55:
-    print("MASTERCAD")
-if c==43:
+    j=j+1
+if cociente==34 or cociente==37:
+    print("American Express")
+if cociente==51 or cociente==55:
+    print("Mastercard")
+if cociente==43:
     print("VISA")
-    
-print("Su codigo de seguridad es:",cvc)#<>
-
-
-#DNI=22555444
-#TARJETA CREDITO= 4348577015644896
-
